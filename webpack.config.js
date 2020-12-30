77a50ea7b5a4708fa6f0ff2822acb69ff14a393d
Tomasz Bawor <bawortomasz@gmail.com>
2020-12-30 13:57:22 +0100

    Add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,7 @@ module.exports = {
             {
                 test: /\.(js|jsx)$/,
                 exclude: "/node_modules/",
-                use: {
-                    loader: 'babel-loader',
-                },
+                use: ['babel-loader', 'eslint-loader']
             },
         ]
     },
